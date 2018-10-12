d2e99fffc4ec001ace270e876ee45f64b4921aef
AkbarKKGithub <41142271+AkbarKKGithub@users.noreply.github.com>
2018-10-12 15:58:47 +0530

    Add files via upload

---
+++ UltraSonicSensor.ino
@@ -0,0 +1,28 @@
+/*
+ * ------ArduinoMakerCamp-------
+ * ------iedcMeenangadi---------
+ * 
+ */
+
+float distance, duration  ;
+int trig=11,echo=10 ;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(trig,LOW);   
+  delay(2);
+  digitalWrite(trig,HIGH);
+  delay(10);
+  digitalWrite(trig,LOW);
+  duration = pulseIn(echo,HIGH);
+  distance = (duration/2)/29.1;
+  Serial.println(distance);
+  delay(100);
+ 
+}
